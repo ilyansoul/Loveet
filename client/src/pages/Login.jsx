0c28cace1ee7385e8c3dff273c458cd240d5aa9a
ilyansoul <ilyan.kortbi.ifcha@gmail.com>
2023-03-29 02:43:25 +0200

    first commit2

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,88 +1,82 @@
-import React from 'react'
-import '../components/styles/login.css'
-import { useNavigate } from 'react-router-dom'
-import { useCookies } from 'react-cookie'
-import { useState } from 'react'
-import axios from 'axios'
 
-
+import React from 'react';
+import '../components/styles/login.css';
+import { useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+import { useState } from 'react';
+import axios from 'axios';
 
 function Login() {
-    const [email, setEmail] = useState(null)
-    const [password, setPassword] = useState(null)
-    const [error, setError] = useState(null)
-    const [cookies, setCookie, removeCookie] = useCookies(null)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [cookies, setCookie] = useCookies(['AuthToken', 'UserId']);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+  const [isLoading, setIsLoading] = useState(false);
 
-        try {
-          
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-            const response = await axios.post('https://loveetback.onrender.com/login', { email, password })
+    setIsLoading(true);
 
-            setCookie('AuthToken', response.data.token)
-            setCookie('UserId', response.data.userId)
+    try {
+        const response = await axios.post('https://loveetback.onrender.com/login', { email, password })
 
-            const success = response.status === 201
-            if (success) navigate('/homepage')
-            if (!success) navigate('/acceuil')
 
-            window.location.reload()
+      setCookie('AuthToken', response.data.token);
+      setCookie('UserId', response.data.userId);
 
-        } catch (error) {
-            console.log(error)
-        }
-
+      const success = response.status === 201;
+      if (success) navigate('/homepage');
+      if (!success) navigate('/acceuil');
+    } catch (error) {
+      setError('Invalid email or password');
+    } finally {
+      setIsLoading(false);
     }
+  };
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
+  return (
+    <div className="front">
+      <form onSubmit={handleSubmit}>
+        <div class="box">
+          <div class="form">
+            <h2>Connexion</h2>
+            <div class="inputBox">
+              <input
+                type="string"
+                name="email"
+                required={true}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <span>Email</span>
+              <i></i>
+            </div>
+            <div class="inputBox2">
+              <input
+                type="password"
+                name="password"
+                required={true}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <span>Mot de passe</span>
+              <i></i>
+            </div>
 
-
-    return (
-        <div className='front'>
-            <form onSubmit={handleSubmit}>
-                <div class="box">
-                <div class="form" >
-                <h2>Connexion</h2>
-                <div class="inputBox">
-                    <input 
-                    type="string" 
-                    name='email' 
-                    required={true}
-                    onChange={(e) => setEmail(e.target.value)}
-                    />
-                    <span>Email</span>
-                    <i></i>
-                </div>
-                <div class="inputBox2">
-                    <input 
-                    type="password" 
-                    name='password' 
-                    required={true}
-                                onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <span>Mot de passe</span>
-                    <i></i>
-                </div>
-
-                        
-                        <div class="links">
-                            <a href="/">Mot de passe oublie?</a>
-                            <a href="/">Inscription</a>
-                        </div>
-                        <input className="btn-login" type="submit" />
-                        <p>{error}</p>
-
-                    </div>
-                </div>
-            </form>
-
-
-
-
-        </div>)
+            <div class="links">
+              <a href="/">Mot de passe oublie?</a>
+              <a href="/">Inscription</a>
+            </div>
+            <input className="btn-login" type="submit" />
+            {isLoading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
